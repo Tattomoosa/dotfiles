95b0a99e2efd1ab8b65ad3f36035b4ae59f3e30a
Matt O'Tousa <mlotousa@gmail.com>
2021-03-03 16:47:05 -0800

    warning & comments

--- .github/README.md
+++ .github/README.md
@@ -1,7 +1,12 @@
 # Dotfiles
 
-This is a bare repo. To clone the easy way, run the [install script](./install.sh):
+## Getting Started
+
+The easy way is to run the [install script](./install.sh):
+
+> Do not trust me. Read the script first. It is very small. It has comments.
 
 ```
-  $ bash <(https://raw.githubusercontent.com/Tattomoosa/dotfiles/main/.github/install.sh)
+  $ bash <(curl https://raw.githubusercontent.com/Tattomoosa/dotfiles/main/.github/install.sh)
 ```
+
--- .github/install.sh
+++ .github/install.sh
@@ -2,19 +2,30 @@
 
 # Based heavily on the script at the end of the fantastic https://www.atlassian.com/git/tutorials/dotfiles
 
-REPO=git@github.com:Tattomoosa/dotfiles.git
+# Change the repo, change the dotfiles
+REPO_REMOTE=git@github.com:Tattomoosa/dotfiles.git
+REPO_LOCAL=.dotfiles
 
+# Clone the repo into .dotfiles
 git clone --bare $REPO $HOME/.dotfiles
+
+# Create a dotfiles command to interact with the repo globally
 function dotfiles {
-	   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
-   }
-mkdir -p .dotfiles-backup
+	/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+# Checkout
 dotfiles checkout
 if [ $? = 0 ]; then
 	echo "Checked out config.";
 else
+	# If checkout failed, move any existing files and try again
 	echo "Backing up pre-existing dot files.";
-	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+	# Create a folder for potential backups
+	mkdir -p $HOME/.dotfiles-backup
+	dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.dotfiles-backup/{}
+	dotfiles checkout
 fi;
-dotfiles checkout
+
+# Do not show untracked files in this repo
 dotfiles config status.showUntrackedFiles no
